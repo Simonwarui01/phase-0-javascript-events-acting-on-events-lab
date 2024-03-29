4bb7dc20566b99f56842a79ffec5a59e34c675f8
Simon <simonwarui93@gmail.com>
2024-03-29 22:41:32 +0300

    assigment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const leftPosition = parseInt(dodger.style.left.replace('px', ''), 10);
+    if (leftPosition > 0) {
+      dodger.style.left = `${leftPosition - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const leftPosition = parseInt(dodger.style.left.replace('px', ''), 10);
+    if (leftPosition < 360) {
+      dodger.style.left = `${leftPosition + 1}px`;
+    }
+  }
